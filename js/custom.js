838b3363b3554a44fc6e97ad274e728ca3ff7af0
nareshthemaverick <nareshthemaverick@gmail.com>
2021-11-30 19:25:39 +0530

    added video

--- js/custom.js
+++ js/custom.js
@@ -2,38 +2,50 @@ const loaderEl = document.getElementsByClassName('fullpage-loader')[0];
 document.addEventListener('readystatechange', (event) => {
 	// const readyState = "interactive";
 	const readyState = "complete";
-    
-	if(document.readyState == readyState) {
+
+	if (document.readyState == readyState) {
 		// when document ready add lass to fadeout loader
-		
-		
+
+
 		// when loader is invisible remove it from the DOM
-		setTimeout(()=>{
+		setTimeout(() => {
 			loaderEl.classList.add('fullpage-loader--invisible');
 			loaderEl.parentNode.removeChild(loaderEl);
 		}, 2000)
 	}
 });
 
-$(document).ready(function() {
-	$('.menu li:has(ul)').click(function(e) {
+$(document).ready(function () {
+	$('.menu li:has(ul)').click(function (e) {
 		e.preventDefault();
 		// console.log(this)
-		if($(this).hasClass('activado')) {
+		if ($(this).hasClass('activado')) {
 			// $(this).removeClass('activado');
-			
+
 			// $(this).children('ul').slideUp();
 		} else {
 			$('.menu li ul').slideUp();
 			$('.menu li').removeClass('activado');
-			$('.menu li').children('a').children('i').attr('class','fa fa-chevron-down')
+			$('.menu li').children('a').children('i').attr('class', 'fa fa-chevron-down')
 			$(this).addClass('activado');
-			$(this).children('a').children('i').attr('class','fa fa-chevron-up')
+			$(this).children('a').children('i').attr('class', 'fa fa-chevron-up')
 			$(this).children('ul').slideDown();
 		}
 
-		$('.menu li ul li a').click(function() {
+		$('.menu li ul li a').click(function () {
 			window.location.href = $(this).attr('href');
 		})
 	});
 });
+
+function openNav() {
+	var myVideo = document.getElementById("video");
+	myVideo.play()
+	document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+	var myVideo = document.getElementById("video");
+	myVideo.pause()
+	document.getElementById("myNav").style.width = "0%";
+  }
